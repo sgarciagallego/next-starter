1c1d1fba96860ab1471a2cd49b65b144f88a5b8a
sgarciagallego <gallegogarciasergio@gmail.com>
2025-04-15 00:41:23 +0100

    feat(sandpack): creating custom functional and accessible sandpack playground

---
+++ src/lib/sandpack.ts
@@ -0,0 +1,48 @@
+import type { SandpackTheme } from '@codesandbox/sandpack-react';
+import { css } from '@linaria/core';
+
+export const theme: SandpackTheme = {
+  colors: {
+    surface1: '#282c34',
+    surface2: '#21252b',
+    surface3: '#2c313c',
+    clickable: '#a8b1c2',
+    base: '#a8b1c2',
+    disabled: '#4d4d4d',
+    hover: '#e8effc',
+    accent: '#c678dd',
+    error: '#e06c75',
+    errorSurface: '#ffeceb',
+  },
+  syntax: {
+    plain: '#a8b1c2',
+    comment: {
+      color: '#757575',
+      fontStyle: 'italic',
+    },
+    keyword: '#c678dd',
+    tag: '#e06c75',
+    punctuation: '#a8b1c2',
+    definition: '#62aeef',
+    property: '#d19a66',
+    static: '#a8b1c2',
+    string: '#98c379',
+  },
+  font: {
+    body: 'var(--font-typeface--sans)',
+    mono: 'var(--font-typeface--mono)',
+    size: '15px',
+    lineHeight: '27px',
+  },
+};
+
+export const hidden = css`
+  display: none;
+`;
+
+export const block = css`
+  display: block;
+`;
+
+export const sandpackWrapper = css``;
+export const sandpackLayout = css``;
